d13306c8c4f9dcad2de2ea0e3e89e15593fd84b7
vero <vero@busybusy.com>
2017-03-24 16:31:13 -0600

    send and receive

--- src/QueueInterface.php
+++ src/QueueInterface.php
@@ -3,8 +3,12 @@ declare(strict_types=1);
 
 namespace Bws\QueueSystem;
 
+use Aws\Result;
+
 
 interface QueueInterface
 {
+	public function sendMessage(string $payload);
 
+	public function retrieveMessage(string $queueUrl) : Result;
 }
--- src/QueueSystem.php
+++ src/QueueSystem.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Bws\QueueSystem;
 
 
+use Aws\Result;
 use Aws\Sqs\SqsClient;
 
 
@@ -14,25 +15,63 @@ use Aws\Sqs\SqsClient;
  */
 class QueueSystem implements QueueInterface
 {
+	/**
+	 * @var SqsClient
+	 */
 	protected $client;
+
+	/**
+	 * @var array
+	 */
+	protected $queues = null;
 
 	public function __construct(SqsClient $client)
 	{
+		assert(func_num_args() === 1);
+
 		$this->client = $client;
+		$this->initiateQueueList();
 	}
 
-	public function getQueueList() : array
+	public function getQueues() : array
 	{
-		return $this->client->listQueues();
+		return $this->queues;
 	}
 
-	public function sendMessage(array $message)
+	public function sendMessage(string $payload)
 	{
+		assert(func_num_args() === 1);
 
+		$message = [];
+		$message['QueueUrl'] = $this->getQueueUrl();
+		$message['MessageBody'] = $payload;
+
+		$this->client->sendMessage($message);
 	}
 
-	public function retrieveMessage()
+	public function retrieveMessage(string $queueUrl) : Result
 	{
+		assert(func_num_args() === 1);
+
+		return $this->client->receiveMessage(['QueueUrl' => $queueUrl]);
+	}
+
+	protected function initiateQueueList()
+	{
+		assert(func_num_args() === 0);
+
+		if ($this->queues === null) {
+			$this->queues = $this->client->listQueues()->get('QueueUrls');
+		}
+	}
+
+	protected function getQueueUrl() : string
+	{
+		assert(func_num_args() === 0);
+
+		$int = rand(0, count($this->queues) - 1);
+
+		return $this->queues[$int];
 
 	}
 }
